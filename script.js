322dc6c3eb390b731c67e3ccf78e0651c0eb180b
Shuang <131367380+Shuang394@users.noreply.github.com>
2024-07-23 13:32:22 +0800

    Added id to the book constructor for easier deletion and addition of books

--- script.js
+++ script.js
@@ -1,15 +1,47 @@
-function Book(author, title, pages, read){
-    this.author = author;
+function Book(author, title, pages, read, id){
+    this.author = author;a
     this.title = title;
     this.pages = pages;
     this.read = read;
+    this.id = id;
 }
 
-let book1 = new Book("James", "The wolf of wallstreet", 236, false);
-let book2 = new Book("Sarah", "The walking Dead", 333, false);
-let book3 = new Book("John", "Absolute Theater", 403, true);
-const myLibrary = [book1, book2, book3];
+function addToCatalog(book){
+    let div = document.createElement("div");
+    div.style.cssText = "display: flex; flex-direction: column; justify-content: center; align-content: center";
 
+    let img = document.createElement("img");
+    img.src = "Images/pexels-mccutcheon-1148399.jpg";
+    img.style.width = "auto";
+    img.style.height = "200px";
+
+    div.appendChild(img);
+
+    let div2 = document.createElement("div");
+    div2.style.cssText = "display: flex; flex-direction: column";
+
+    let author = document.createElement("p");
+    author.textContent = "Name of the author: " + book.author;
+
+    let title = document.createElement("p");
+    title.textContent = "Title of the book: " + book.title;
+
+    let pages = document.createElement("p");
+    pages.textContent = "Number of pages: " + book.pages;
+
+    div2.appendChild(author);
+    div2.appendChild(title);
+    div2.appendChild(pages);
+    
+    div.appendChild(div2);
+    catalog.appendChild(div);
+}
+
+let book1 = new Book("James", "The wolf of wallstreet", 236, false, 0);
+let book2 = new Book("Sarah", "The walking Dead", 333, false, 1);
+let book3 = new Book("John", "Absolute Theater", 403, true, 2);
+const myLibrary = [book1, book2, book3];
+const catalog = document.querySelector(".catalog");
 
 function addBookToLibrary(){};
 
@@ -25,12 +57,14 @@ uploadButton.addEventListener('click', function() {
     const x = author.value;
     const y = title.value;
     const z = pages.value;
+
     console.log(x + y + z);
-    myLibrary[id] = new Book(x, y, z, false);
+    myLibrary[id] = new Book(x, y, z, false, id);
     id++;
-    console.log(myLibrary);
+    
 }
 )
 
-
-
+for (i in myLibrary){
+    addToCatalog(myLibrary[i]);
+}
